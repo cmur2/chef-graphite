d2603afd77cf2a73e8ec96a936fa3145f43612f7
cn <chrnicolai@gmail.com>
2013-05-09 15:46:03 +0200

    Fix typo

--- libraries/graphite.rb
+++ libraries/graphite.rb
@@ -1,7 +1,8 @@
 
 class Chef::Node
   def generate_local_settings
-    return nil if if self['graphite']['webapp']['local_settings'].nil?
+    return nil if self['graphite']['webapp']['local_settings'].nil?
+    
     lines = []
     self['graphite']['webapp']['local_settings'].each do |name, value|
       lines << "#{name} = #{value}"
