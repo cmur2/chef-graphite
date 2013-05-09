5446ae8f2a78d1dab20312d2907b783b40f58e12
cn <chrnicolai@gmail.com>
2013-05-09 15:42:09 +0200

    Generate local_settings from strings without type suppot

--- libraries/graphite.rb
+++ libraries/graphite.rb
@@ -1,5 +1,12 @@
+
 class Chef::Node
   def generate_local_settings
-    ''
+    return nil if if self['graphite']['webapp']['local_settings'].nil?
+    lines = []
+    self['graphite']['webapp']['local_settings'].each do |name, value|
+      lines << "#{name} = #{value}"
+    end
+    lines << ''
+    lines.join "\n"
   end
 end
