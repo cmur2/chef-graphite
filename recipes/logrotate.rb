e33a31d1c266429fdfa26069e6a3b06fafa664e3
cn <cn@mycrobase.de>
2013-05-10 21:10:58 +0200

    Fix

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -10,6 +10,7 @@ begin
     
     execute "apply-disable-internal-logrotate-patch" do
       command "patch -b -p1 -i /opt/graphite/disable_internal_logroate.patch"
+      cwd "/opt/graphite"
       not_if { ::File.exists?("/opt/graphite/lib/carbon/log.py.orig") }
     end
     
