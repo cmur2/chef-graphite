b49c91c975efb335735b40ad0100fd41852f8b6a
cn <cn@mycrobase.de>
2013-05-11 13:10:50 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
 description      "Installs and configures carbon and/or graphite-webapp (including whisper)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "1.0.0"
 
 depends "python"
 
